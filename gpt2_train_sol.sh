176eddf2de1f55a7d70448435242f9c103abbca4
shuchitalati <145169188+shuchitalati@users.noreply.github.com>
2023-11-25 13:40:12 -0700

    Update env gpt2_train_sol.sh

--- gpt2_train_sol.sh
+++ gpt2_train_sol.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH -p general
-#SBATCH -t 2-00:0:00
+#SBATCH -t 5-00:0:00
 #SBATCH --mem=100GB
-#SBATCH -G a100:1
+#SBATCH -G a100:2
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=phegde7@asu.edu
+#SBATCH --mail-user=stalati1@asu.edu
 #SBATCH -o slurm.%j.out # file to save job's STDOUT (%j = JobId)
 #SBATCH -e slurm.%j.err # file to save job's STDERR (%j = JobId)
 #SBATCH --export=NONE
 
 module purge
 module load mamba/latest
-source activate attention_guidance
+source activate /scratch/phegde7/.conda/envs/attention_guidance_v2/
 
 python gpt2_train.py
